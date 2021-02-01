ae31c735d3ddcf2212480c00c9a277aa3d37ace5
alinosratipour <74776647+alinosratipour@users.noreply.github.com>
2021-02-01 03:05:19 +0000

    exersice C-array-every Completed

--- 1-exercises/C-array-every/exercise.js
+++ 1-exercises/C-array-every/exercise.js
@@ -5,7 +5,11 @@
 var students = ["Omar", "Austine", "Dany", "Swathi", "Lesley", "Rukmini"];
 var group = ["Austine", "Dany", "Swathi", "Daniel"];
 
-var groupIsOnlyStudents; // complete this statement
+function checkStudent(n){
+  return  students.includes(n);
+}
+
+ var groupIsOnlyStudents = group.every(checkStudent); // complete this statement
 
 if (groupIsOnlyStudents) {
   console.log("The group contains only students");
