651bdd61ceef4cc75938b1064474ef2caa7c87f3
alinosratipour <74776647+alinosratipour@users.noreply.github.com>
2021-02-04 18:08:31 +0000

    6-lane-names completed

--- 2-mandatory/6-lane-names.js
+++ 2-mandatory/6-lane-names.js
@@ -6,7 +6,10 @@
   HINT: string and array methods that could be helpful (indexOf, filter)
 */
 
-function getLanes() {}
+function getLanes(hasLane) {
+  return hasLane.filter( street => 
+  street.includes("Lane"));
+}
 
 /* ======= TESTS - DO NOT MODIFY ===== */
 
